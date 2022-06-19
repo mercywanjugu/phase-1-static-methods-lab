c67c139ac0c6652d0040b12f5b9c13662fbf6608
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-19 04:02:23 -0400

    commit tree cf95404d3055048d2d8c6118e88de837da5c5231

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1,)
+  }
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+  static titleize(str) {
+    let strr = str.split(' ')
+    let newSent = strr.map(function(word) {
+    if (word === 'the') { return word }
+    else if (word === 'a')  { return word }
+    else if (word === 'an')  { return word }
+    else if (word === 'but') { return word }
+    else if (word === 'of')  { return word }
+    else if (word === 'and')  { return word }
+    else if (word === 'for') { return word }
+    else if (word === 'at') { return word }
+    else if (word === 'by') { return word }
+    else if (word === 'from') { return word }
+    else { return word.charAt(0).toUpperCase() + word.slice(1,) }
+
+    //the, a, an, but, of, and, for, at, by, and from
+    })
+    let newWord = ""
+    for (let i=0; i < newSent.length; i++) {
+      newWord += newSent[i]
+      if (i !== (newSent.length - 1)) { newWord += " " }
+    }
+    newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1,)
+    return newWord
+  }
 }
+
+let a = Formatter.titleize("a tale of two cities")
+console.log(a)
